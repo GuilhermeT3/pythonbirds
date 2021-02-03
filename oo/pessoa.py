973769dae2032fb9fc25899bef8aad704a999317
Guilherme <telesguilherme84@gmail.com>
2021-02-03 19:44:52 -0300

    criados atributps de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=17):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Teles')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Guilherme'
+    print(p.nome)
+    print(p.idade)
